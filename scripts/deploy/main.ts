b2501cd61b0e8dfc841a85eb9bd5ed753c8cf777
Teerawut Saesim <55015430+ribbinpo@users.noreply.github.com>
2024-01-12 17:39:46 +0700

    Initial commit

---
+++ scripts/deploy/main.ts
@@ -0,0 +1,12 @@
+import { deployLogger } from "./deployLogger";
+
+async function main() {
+  const lock = await deployLogger();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
